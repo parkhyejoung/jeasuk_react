0300317bb7f15f6290c5e8f70f5ba43e65f74efc
hyejoung <qkrgpwjd1996@gmail.com>
2025-07-07 12:00:51 +0900

    유재석소개페이지

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+import pic3 from '../assets/images/jaeseok3.png'
+
+function Intro(){
+    return(<>
+    <p>
+        - 1972년 8월 14일
+        - 서울특별시 성북구 수유동 출생
+        - 국적 : 대한민국 / 종교 : 불교
+        - 178cm, 65kg, B형, 270mm, ISFP
+        - 서울예술전문대학 방송연예과 중퇴
+        <img src={pic3} alt="" />
+    </p>
+    </>)
+}
+export default Intro;
